9f3471c3e4de0931849f08005ec830b7abaf73bd
Mary <maryast1@yandex.ru>
2023-12-28 16:26:47 +1200

    added task3

--- Program.cs
+++ Program.cs
@@ -104,3 +104,71 @@
 // PrintArray();
 // ChangeArray();
 // PrintArray();
+
+
+
+
+
+// Задача 3
+
+// Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить 
+// строку с наименьшей суммой элементов.
+
+
+int [,] array = new int [5,3];
+
+void CreateArray()
+{
+    for (int i=0; i<array.GetLength(0); i++ )
+    {
+        for (int j=0; j<array.GetLength(1); j++ )
+        {
+            array[i, j]=new Random().Next(1, 10);
+        }
+    }
+}
+void PrintArray()
+{
+    for (int i=0; i<array.GetLength(0); i++ )
+    {
+        for (int j=0; j<array.GetLength(1); j++ )
+        {
+            System.Console.Write (array[i,j] + " ");
+        }
+        System.Console.WriteLine ();
+    }
+    
+}
+
+void SumStringArray(int[,] array)
+{
+    int index = 0, minsum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        Console.WriteLine($"Сумма {i + 1} строки = {sum}");
+        if (i == 0)
+        {
+            minsum = sum;
+        }
+        else if (sum < minsum)
+        {
+            minsum = sum;
+            index = i;
+        }
+    }
+    string line = string.Empty;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        line += array[index, j] + " ";
+    }
+    Console.WriteLine($"Минимальная сумма элементов строки равна {minsum}, а ее индекс - {index}");
+}
+CreateArray();
+PrintArray();
+SumStringArray(array);
