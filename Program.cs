b28b0fcec270b440a9f641d1a51a46d33b858857
Mary <maryast1@yandex.ru>
2023-12-28 16:25:52 +1200

    added task2

--- Program.cs
+++ Program.cs
@@ -56,4 +56,51 @@
 
 
 
+// Задача 2:
+// Задайте двумерный массив. 
+// Напишите программу, которая поменяет 
+// местами первую и последнюю строку массива.
 
+// int [,] array = new int [7,4];
+
+// void CreateArray()
+// {
+//     for (int i=0; i<array.GetLength(0); i++ )
+//     {
+//         for (int j=0; j<array.GetLength(1); j++ )
+//         {
+//             array[i, j]=new Random().Next(1, 10);
+//         }
+//     }
+// }
+// void PrintArray()
+// {
+//     for (int i=0; i<array.GetLength(0); i++ )
+//     {
+//         for (int j=0; j<array.GetLength(1); j++ )
+//         {
+//             System.Console.Write (array[i,j] + " ");
+//         }
+//         System.Console.WriteLine ();
+//     }
+    
+// }
+
+// void ChangeArray ()
+// {
+//     for (int i = 0; i < array.GetLength(1); i++)
+    
+//         {
+//             int temporary = array[0, i];
+//             array[0, i] = array[array.GetLength(0)-1, i];
+//             array[array.GetLength(0)-1, i] = temporary;
+//         }
+//     System.Console.WriteLine ();
+//     System.Console.WriteLine ("Массив, где первую строчку поменяли местом с последней: ");
+// } 
+    
+
+// CreateArray();
+// PrintArray();
+// ChangeArray();
+// PrintArray();
